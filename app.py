7620d861b205617281607dd2d0c4710d0da9e530
bc-rep-project <johannderbiel@gmail.com>
2025-02-07 14:54:00 +0200

    Update Gunicorn and app configuration to use gunicorn_config.py

--- app.py
+++ app.py
@@ -207,5 +207,5 @@ def delete_repository(repo_id):
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port, debug=False)
