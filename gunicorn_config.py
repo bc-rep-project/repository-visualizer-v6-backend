7924f19ae398c83969e39dff2f749288e2d6d26c
bc-rep-project <johannderbiel@gmail.com>
2025-02-07 22:29:12 +0200

    Update Gunicorn configuration to use environment variables for workers and port

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,7 @@
 import os
 
 # Worker configuration
-workers = 4
+workers = int(os.environ.get("GUNICORN_WORKERS", "3"))
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 120
@@ -21,4 +21,7 @@ reload = False
 
 # SSL
 keyfile = None
-certfile = None 
+certfile = None
+
+# Update bind address to use PORT environment variable with 8000 as fallback
+bind = "0.0.0.0:" + os.environ.get("PORT", "8000") 
